a6a5b2518c6f6e75dff6e721fe44a06d4fd4fccb
Thiago Saraiva <thiago.a.saraiva@aexp.com>
2019-02-02 17:30:36 +0000

    adds and customize SearchView to MainActivity

--- app/src/main/java/com/example/tsaraiva/cryptocurrencycatalog/ui/MainActivity.kt
+++ app/src/main/java/com/example/tsaraiva/cryptocurrencycatalog/ui/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.SearchView
 import android.support.v7.widget.Toolbar
 import android.view.Menu
 import android.view.MenuItem
@@ -33,18 +34,33 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.crypto_currencies_list_menu, menu)
-        return true
+
+        val searchMenuItem = menu?.findItem(R.id.action_search) as MenuItem
+
+        searchMenuItem.setOnActionExpandListener(object : MenuItem.OnActionExpandListener {
+            override fun onMenuItemActionExpand(item: MenuItem?): Boolean {
+                Toast.makeText(this@MainActivity, "Search View Opened!", Toast.LENGTH_SHORT).show()
+                return true
+            }
+
+            override fun onMenuItemActionCollapse(item: MenuItem?): Boolean {
+                Toast.makeText(this@MainActivity, "Search View Closed!", Toast.LENGTH_SHORT).show()
+                return true
+            }
+        })
+
+        // Configure the search info and add any event listeners...
+        val searchView = searchMenuItem?.actionView as SearchView
+        searchView.queryHint = "Search for crypto..."
+
+        return super.onCreateOptionsMenu(menu)
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
         return when (item?.itemId) {
-            R.id.action_search -> {
-                Toast.makeText(this, "Search Action!", Toast.LENGTH_SHORT).show()
-                startActivity(SecondActivity.getIntent(this))
-                true
-            }
             R.id.action_filter -> {
                 Toast.makeText(this, "Search Filter!", Toast.LENGTH_SHORT).show()
+                startActivity(SecondActivity.getIntent(this))
                 true
             }
             else -> super.onOptionsItemSelected(item)
