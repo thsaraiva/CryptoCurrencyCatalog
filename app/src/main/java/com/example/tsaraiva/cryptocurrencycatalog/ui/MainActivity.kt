a8a81d69cdc76274ea1b0379b3651fb9839f1a74
Thiago Saraiva <thiago.a.saraiva@aexp.com>
2019-02-02 09:06:07 +0000

    Adds v7-appcompat Toolbar as appbar with search and filter icon

--- app/src/main/java/com/example/tsaraiva/cryptocurrencycatalog/ui/MainActivity.kt
+++ app/src/main/java/com/example/tsaraiva/cryptocurrencycatalog/ui/MainActivity.kt
@@ -4,6 +4,11 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.Toolbar
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
 import com.example.tsaraiva.cryptocurrencycatalog.R
 import com.example.tsaraiva.cryptocurrencycatalog.model.Currency
 import com.example.tsaraiva.cryptocurrencycatalog.ui.utils.CurrencyJSONConverter
@@ -16,10 +21,34 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val toolbar = findViewById<Toolbar>(R.id.my_toolbar)
+        setSupportActionBar(toolbar)
+
+        supportActionBar?.title = "Crypto Currency List"
+
         val recyclerView = findViewById<RecyclerView>(R.id.currencies_list)
         recyclerView.setHasFixedSize(true)
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.adapter = CurrenciesAdapter(getCurrencyDataFromLocalJSONFile() ?: listOf())
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.crypto_currencies_list_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return when (item?.itemId) {
+            R.id.action_search -> {
+                Toast.makeText(this, "Search Action!", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.action_filter -> {
+                Toast.makeText(this, "Search Filter!", Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
     private fun getCurrencyDataFromLocalJSONFile(): List<Currency>? {
